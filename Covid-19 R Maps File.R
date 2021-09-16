b5ae8b79ff79cfe1171788d9d699da09a1e95d32
Megha Patel <mgh.patel005@gmail.com>
2021-09-16 12:50:56 -0400

    Update Covid-19 R Maps File.R

--- Covid-19 R Maps File.R
+++ Covid-19 R Maps File.R
@@ -3,8 +3,8 @@
 #https://github.com/nytimes/covid-19-data
 
 #Change filepath
-us.states.csv <- read.csv("C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/us-states.csv.txt")
-us.counties.csv<-read.csv("C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/us-counties.csv.txt")
+us.states.csv <- read.csv("INSERT FULL FILE PATH for us-states.csv.txt")
+us.counties.csv<-read.csv("INSERT FULL FILE PATH for us-counties.csv.txt")
 
 #Declare Columns as vectors. 
 Date<-us.states.csv[1]
@@ -85,7 +85,7 @@ sapply(us_map_data_2, mean, na.rm=TRUE)
 #This code sums the death columns automatically.Some images use scientific notation to report cases. Please note that the scale map_df[,values] does not display the number of deaths/cases correctly in the 
 #most updated file. This program was written in May 2020 when the cases/death numbers were not quite as large.As a result, the color of the graphs are off when using the most updated data set
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/Northeast Region Death.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/Northeast Region Death.jpeg", width = 1050, height = 1050)
 map1<-plot_usmap(data=us_map_data, values="deaths", color="white", include = .northeast_region, labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Deaths")
 map1
 dev.off()
@@ -93,56 +93,56 @@ dev.off()
 map1
 
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/East North Central.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/East North Central.jpeg", width = 1050, height = 1050)
 map2<-plot_usmap(data=us_map_data, values="deaths", color="white", include = .east_north_central, labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Deaths")  
 map2
 dev.off()
 
 map2
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/Northeast Region Cases.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/Northeast Region Cases.jpeg", width = 1050, height = 1050)
 map3<-plot_usmap(data=us_map_data, values="cases", color="white", include = .northeast_region, labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Cases") 
 map3
 dev.off()
 
 map3
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/Mid Atlantic Death.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/Mid Atlantic Death.jpeg", width = 1050, height = 1050)
 map4<-plot_usmap(data=us_map_data, values="deaths", color="white", include = .mid_atlantic, labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Deaths")
 map4
 dev.off()
 
 map4
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/Mid Atlantic Cases.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/Mid Atlantic Cases.jpeg", width = 1050, height = 1050)
 map5<-plot_usmap(data=us_map_data, values="cases", color="white", include = .mid_atlantic, labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Cases")
 map5
 dev.off()
 
 map5
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/NJ Cases.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/NJ Cases.jpeg", width = 1050, height = 1050)
 map6<-plot_usmap(data=us_map_data_2, values="cases", region="counties", include = "NJ",labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Cases")
 map6
 dev.off()
 
 map6
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/NJ Deaths.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/NJ Deaths.jpeg", width = 1050, height = 1050)
 map7<-plot_usmap(data=us_map_data_2, values="deaths", region="counties", include = "NJ",labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Deaths")
 map7
 dev.off()
 
 map7
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/US Deaths.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/US Deaths.jpeg", width = 1050, height = 1050)
 map8<-plot_usmap(data=us_map_data, values="deaths", color="white", labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Deaths")
 map8
 dev.off()
 
 map8
 
-jpeg(file = "C:/Users/patel_m/OneDrive - US Department of Labor - BLS/Desktop/Presentation/US Cases.jpeg", width = 1050, height = 1050)
+jpeg(file = "INSERT FULL FILE PATH for saving image/US Cases.jpeg", width = 1050, height = 1050)
 map9<-plot_usmap(data=us_map_data, values="cases", color="white", labels=TRUE,label_color="white")+ theme(legend.position="top")+labs(color="Cases")
 map9
 dev.off()
